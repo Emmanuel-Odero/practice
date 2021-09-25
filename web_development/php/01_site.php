34040a0530521a1ac6d49aa1cff0fc34c9c2de35
ochara <michaelteddy009@gmail.com>
2021-09-25 10:51:05 +0300

    another update

---
+++ web_development/php/01_site.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First php</title>
+</head>
+<body>
+	<?php 
+		echo("<h1>Hello World</h1>");
+		echo"<hr>";
+		echo("This is my site");
+	?>
+</body>
+</html>
